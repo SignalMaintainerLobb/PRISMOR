707de336867cf81b8827ab49c21264baa9b95210
SignalMaintainerLobb <warrenlobb@gmail.com>
2025-07-09 14:53:12 -0400

    Delete background.js

--- background.js
+++
@@ -1,7 +0,0 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === "openTab") {
-    chrome.tabs.create({
-      url: chrome.runtime.getURL("popup.html")
-    });
-  }
-});
